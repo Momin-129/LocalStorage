ae500a32d9342156ceae9558f6816f8f9d99f853
Momin Rather <momin.rather@gmail.com>
2023-05-08 14:08:54 +0530

    Changed Display pattern

--- js/app.js
+++ js/app.js
@@ -4,30 +4,18 @@ const form = document.querySelector("form");
 // Show Data
 document.getElementById("showBtn").addEventListener("click", () => {
   let details = document.getElementById("showDetails");
-  let row = document.getElementById("gridRow");
-  row.innerHTML = "";
+  let table = document.getElementById("userDetails");
+  // row.innerHTML = "";
 
   record = JSON.parse(localStorage.getItem("record"));
 
   for (eachRecord of record) {
-    let col = document.createElement("div");
-    col.classList.add("col-md-4", "col-sm-12");
-    row.appendChild(col);
-    let individual = document.createElement("div");
-    individual.classList.add("individual");
-    col.appendChild(individual);
-    let table = document.createElement("table");
-    individual.appendChild(table);
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
     for (item in eachRecord) {
-      let tr = document.createElement("tr");
-      table.appendChild(tr);
-      let th = document.createElement("th");
       let td = document.createElement("td");
-      let key = document.createTextNode(item.toUpperCase());
       let value = document.createTextNode(eachRecord[item]);
-      th.appendChild(key);
       td.appendChild(value);
-      tr.appendChild(th);
       tr.appendChild(td);
     }
   }
